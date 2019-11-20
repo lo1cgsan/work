86dc00bd38b3746f3fe851f0cb1ed0f151cdd0ef
lo1cgsan <lo1cgsan@gmail.com>
2019-11-20 15:16:55 +0100

    dodanie pilki i początek animacji

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tik201920
+
+github.com/lo1cgsan/tik201920
--- pong.py
+++ pong.py
@@ -30,16 +30,47 @@ paletka2_prost = paletka2.get_rect()
 paletka2_prost.x = P2_POZ[0]
 paletka2_prost.y = P2_POZ[1]
 
+PI_SZER = 20
+PI_WYS = 20
+PI_VX = 5
+PI_VY = 5
+GREEN = (0, 255, 0)
+pilka = pygame.Surface([PI_SZER, PI_WYS], pygame.SRCALPHA, 32)
+pilka.convert_alpha()
+pygame.draw.ellipse(pilka, GREEN, [0, 0, PI_SZER, PI_WYS])
+
+pilka_prost = pilka.get_rect()
+pilka_prost.x = OKNOGRY_SZER / 2
+pilka_prost.y = OKNOGRY_WYS / 2
+
+FPS = 30
+fpsClock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    
+
+        if event.type == MOUSEMOTION:
+            myszaX, myszaY = event.pos
+
+            przesuniecie = myszaX - (P_SZER / 2)
+
+            if przesuniecie > OKNOGRY_SZER - P_SZER:
+                przesuniecie = OKNOGRY_SZER - P_SZER
+            if przesuniecie < 0:
+                przesuniecie = 0
+
+            paletka1_prost.x = przesuniecie
+
+    pilka_prost.move_ip(PI_VX, PI_VY)
+
     oknogry.fill(KOLOR_BLUE)
-    
+
     oknogry.blit(paletka1, paletka1_prost)
     oknogry.blit(paletka2, paletka2_prost)
-    
+    oknogry.blit(pilka, pilka_prost)
+
     pygame.display.update()
+    fpsClock.tick(FPS)
