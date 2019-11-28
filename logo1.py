5b4e2a988092b5429e9a0c0231a25cfa7907382f
lo1cgsan <lo1cgsan@gmail.com>
2019-11-28 15:42:05 +0100

    użycie pętli, random i sekwencji tupli

--- logo1.py
+++ logo1.py
@@ -2,35 +2,49 @@
 # -*- coding: utf-8 -*-
 #  logo1.py
 import turtle
+import math
+import random
 
 
-def kwadrat(bok, x, y):
+def kwadrat(bok, x, y, kolor=(0, 0, 0)):
     turtle.penup()
     turtle.setpos(x, y)
     turtle.pendown()
+    turtle.pencolor(kolor)
     for i in range(4):
         turtle.forward(bok)
         turtle.left(90)
 
 
-def kolo(promien, x=0, y=0):
+def kolo(promien, x=0, y=0, kolor=(0, 0, 0)):
     turtle.penup()
     turtle.setpos(x, y)
     turtle.pendown()
+    turtle.pencolor(kolor)
     turtle.circle(promien)
+
+
+def losujKolor():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
 
 
 def kwadraty():
     ile = 5
     bok = 100
-    krok = 40
+    # krok = 40
 
     for i in range(ile):
-        bok = bok + krok
         x = bok / 2
-        print(i, bok, x)
-        kwadrat(bok, -x, -x)
-        kolo(bok / 2, 0, -bok / 2)
+        kolor = losujKolor()
+        kwadrat(bok, -x, -x, kolor)
+        r = math.sqrt(x**2 + x**2)
+        # kolo(bok / 2, 0, -bok / 2)
+        kolor = losujKolor()
+        kolo(r, 0, -r, kolor)
+        bok = 2 * r
 
 
 def gwiazdy():
@@ -45,13 +59,25 @@ def gwiazdy():
             turtle.right(kat)
 
 
+def rysujKola():
+    r = 50
+    pozycje = [(0, 0), (-120, 0), (60, 60),
+               (-60, 60), (-180, 60)]
+    for x, y in pozycje:
+        kolor = losujKolor()
+        kolo(r, x, y, kolor)
+
+
 def main(args):
     turtle.setup(800, 600)
-    turtle.pencolor("green")
+    turtle.colormode(255)
+    # turtle.pencolor("green")
+    turtle.pencolor(0, 255, 0)
     turtle.pensize(5)
     # kwadrat(100, -50, -50)
     # gwiazdy()
-    kwadraty()
+    # kwadraty()
+    rysujKola()
     turtle.done()
     return 0
 
