64080e0dfe7d5fbe54f22f3e75618e8ad5aaf826
lo1cgsan <lo1cgsan@gmail.com>
2020-04-08 14:23:45 +0200

    funkcja pobierzDane i kontrola wartości

--- python/bmi.py
+++ python/bmi.py
@@ -1,6 +1,18 @@
-m = float(input('Podaj masę ciała (kg): '))
-w = float(input('Podaj wzrost (m): '))
+def pobierzDane():
+    m = float(input('Podaj masę ciała (kg): '))
+    while m < 10 or m > 200:
+        print('Błędna wartość')
+        m = float(input('Podaj masę ciała (kg): '))
 
+    w = float(input('Podaj wzrost (m): '))
+    while w < 1 or w > 2.5:
+        print('Błędna wartość')
+        w = float(input('Podaj wzrost (m): '))
+
+    return m, w
+
+
+m, w = pobierzDane()
 print(m, w)
 
 bmi = m / (w**2)
