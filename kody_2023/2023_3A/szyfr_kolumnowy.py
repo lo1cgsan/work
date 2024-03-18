636a7baa6aea5b5ebde1121b94613be41e5c2803
lo1cgsan <lo1mint@atsbox.lo1cg.org>
2024-03-18 07:28:07 +0100

    kolumnowy

--- kody_2023/2023_3A/szyfr_kolumnowy.py
+++ kody_2023/2023_3A/szyfr_kolumnowy.py
@@ -20,9 +20,22 @@ TOTEORT.BNO.EOB.
 """
 tekst = input("Podaj tekst: ")
 tekst = tekst.upper().replace(" ", "")
-klucz = len(tekst) // 2 - 1
+dl_tekstu = len(tekst)
+klucz = dl_tekstu // 2 - 1
+ile_wierszy, reszta = divmod(dl_tekstu, klucz)
+
+if ile_wierszy * klucz < dl_tekstu:
+    ile_wierszy += 1
+    ile_dodac = ile_wierszy * klucz - dl_tekstu
+    tekst += ile_dodac * '_'
+    print(dl_tekstu, klucz, ile_wierszy, reszta, ile_dodac, tekst)
 
 for i in range(0, len(tekst), klucz):
     for j in range(klucz):
         print(tekst[i+j], end='')
     print()
+
+for i in range(klucz):
+    for j in range(ile_wierszy):
+        print(tekst[klucz * j + i], end='')
+print()
