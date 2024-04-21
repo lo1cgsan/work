0c6f47345961275e8cf206c9664a74b56406b6e0
lo1cgsan <lo1mint@dboxh.lo1cg.org>
2024-04-21 18:24:00 +0200

    1A todo

---
+++ kody_2023/2023_1A_inf/todo.py
@@ -0,0 +1,4 @@
+– znajdowanie mimnimum (n-1 porównań)
+– znajdowanie maksimum (n-1 porównań)
+– jednoczesne znajdowanie minimum i maksimum
+(np. aby obliczyć rozpiętość zbioru =  maksimum - minimum)
