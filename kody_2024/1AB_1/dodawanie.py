109ee2db343fe480d7d91bcc2e639fbd1d3279ae
lo1cgsan <lo1cgsan@gmail.com>
2025-02-07 14:25:37 +0100

    07 luty 2025

---
+++ kody_2024/1AB_1/dodawanie.py
@@ -0,0 +1,9 @@
+# dane wejściowe
+a = 100
+b = -67
+
+# algorytm
+suma = a + b
+
+# wynik: wypisana suma liczb a i b
+print(suma)
---
+++ kody_2024/1AB_2/dodawanie.py
@@ -0,0 +1,9 @@
+# dane wejściowe
+a = 5
+b = 8
+
+# algorytm
+suma = a + b
+
+# wynik, dane wyjściowe
+print(suma)
