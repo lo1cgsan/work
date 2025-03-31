6ffa729673dbd87de34b028543858e2142a1b189
lo1cgsan <lo1cgsan@gmail.com>
2025-03-31 12:16:22 +0200

    wisielec

---
+++ kody_2024/3B_1/wisielec.py
@@ -0,0 +1,32 @@
+slowo = str(input("Podaj słowo do odgadnięcia: "))
+zycia = len(slowo)
+odgadniete = 0
+zgadniete_litery = []
+
+while zycia > 0:
+    odgadywane = str(input("Podaj literę lub całe słowo do sprawdzenia: "))
+    if len(odgadywane) == 1:
+        if odgadywane in slowo:
+            if odgadywane not in zgadniete_litery:
+                print("Słowo zawiera literę: ", odgadywane)
+                odgadniete += slowo.count(odgadywane)
+                zgadniete_litery.append(odgadywane)
+            if odgadniete == len(slowo):
+                break
+        else:
+            print("Słowo nie zawiera litery: ", odgadywane)
+            zycia -= 1
+    else:
+        if odgadywane == slowo:
+            print("Podano poprawne słowo!")
+            odgadniete = len(slowo)
+            break
+        else:
+            print("Podane słowo nie jest hasłem!")
+            zycia -= 1
+    print("Pozostałe życia: ", zycia)
+
+if odgadniete == len(slowo):
+    print("Udało ci się odgadnąć słowo: ", slowo)
+else:
+    print("Niestety nie udało ci się odgadnąć słowa: ", slowo)
