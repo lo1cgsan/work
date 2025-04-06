620ca82e14de50ced1ccf130bf9f6563415c079f
lo1cgsan@gmail.com <lo1cgsan@gmail.com>
2025-04-06 19:38:55 +0200

    Poprawki FLask toDo cd.

--- projekty_flask/todo/modele.sql
+++ projekty_flask/todo/modele.sql
@@ -7,6 +7,7 @@ CREATE TABLE user (
   login TEXT UNIQUE NOT NULL, -- nazwa użytkownika
   haslo TEXT NOT NULL -- hasło użytkownika
 );
+-- przykładowy użytkownik
 INSERT INTO user (id, login, haslo)
 VALUES (null, 'adam', 'scrypt:32768:8:1$b6ySf4OhUqADg4os$9fab79b9175c7e1ac341d06b72a3bb3e3a213733c6211bfa7f2b388988065e837df630be38e7eb5729d59db4f5e7d0abd7886e0697125f1a0e8a0eadd6a9eb3a');
 
