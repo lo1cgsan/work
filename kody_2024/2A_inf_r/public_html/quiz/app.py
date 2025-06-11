8c65493591c0a4ece6609cefd5acc9df7066867b
lo1cgsan@gmail.com <lo1cgsan@gmail.com>
2025-06-11 20:54:39 +0200

    poprawki quiz_pytania

--- kody_2024/2A_inf_r/public_html/quiz/app.py
+++ kody_2024/2A_inf_r/public_html/quiz/app.py
@@ -1,5 +1,5 @@
 from flask import (
-    Flask, render_template, request, redirect, flash, url_for, current_app
+    Flask, render_template, current_app
 )
 import os
 from db import init_app, init_db
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 app.config.update(dict(
     SECRET_KEY='bardzosekretnawartosc',
-    SITE_NAME='Quiz Flask',
+    SITE_NAME='Pytania Flask',
     DATABASE=os.path.join(app.root_path, 'db.sqlite')
 ))
 
@@ -24,34 +24,6 @@ app.register_blueprint(pytania.bp)
 def index():
     return render_template('index.html')
 
-
-dane = [
-    {
-        'pytanie': 'Stolica Hiszpanii to:',
-        'odpowiedzi': ['Warszawa', 'Madryt', 'Barcelona'],
-        'odpok': 'Madryt'
-    },
-    {
-        'pytanie': 'Symbol pierwiastka Helu to:',
-        'odpowiedzi': ['Fe', 'H', 'He'],
-        'odpok': 'He'
-    }
-]
-
-# @app.route('/pytania', methods=['GET', 'POST'])
-# def pytania():
-#     if request.method == 'POST':
-#         print(request.form)
-#         punkty = 0
-#         odpowiedzi = request.form
-#         for p_nr, odp in odpowiedzi.items():
-#             if odp == dane[int(p_nr)]['odpok']:
-#                 punkty += 1
-
-#         flash(f'Liczba poprawnych odpowiedzi: {punkty}')
-#         return redirect(url_for('pytania'))
-
-#     return render_template('pytania.html', pytania=dane)
 
 with app.app_context():
     if not os.path.exists(current_app.config['DATABASE']):
--- kody_2024/2A_inf_r/public_html/quiz/pytania.py
+++ kody_2024/2A_inf_r/public_html/quiz/pytania.py
@@ -1,7 +1,7 @@
 from flask import (
     Blueprint, flash, render_template, request, redirect, url_for
 )
-from db import query_db
+from db import query_db, get_db
 
 
 bp = Blueprint('pytania', __name__, template_folder='pytania', url_prefix='/pytania')
@@ -16,7 +16,7 @@ def lista():
 
 @bp.route('/dodaj', methods=['GET', 'POST'])
 def dodaj():
-    pytanie_form = {'pytanie':'','odpowiedzi':[], 'poprawne':[]}
+    pytanie_form = {'pytanie': '', 'odpowiedzi': [], 'poprawne': []}
     errors = []
 
     if request.method == 'POST':
@@ -25,8 +25,6 @@ def dodaj():
         pytanie = request.form['pytanie'].strip()
         if not pytanie:
             errors.append = 'Nie wpisano pytania!'
-        else:
-            pytanie_form['pytanie'] = pytanie
 
         odpowiedzi = request.form.getlist('odpowiedzi')
         for i, o in enumerate(odpowiedzi):
@@ -38,10 +36,28 @@ def dodaj():
         poprawne = request.form.getlist('poprawne')
         if not len(poprawne):
             errors.append(' Nie zaznaczono przynajmniej jednej poprawnej odpowiedzi!')
-
+        print(poprawne)
         if not errors:
-            pass
+            sql = 'INSERT INTO pytanie VALUES (?, ?)'
+            db = get_db()
+            cur = db.execute(sql, [None, pytanie])
+            # db.commit()
+            pytanie_id = cur.lastrowid
+            sql = 'INSERT INTO odpowiedz VALUES (?, ?, ?, ?)'
+            for i, odp in enumerate(odpowiedzi):
+                poprawna = False
+                if str(i+1) in poprawne:
+                    poprawna = True
+                print([None, pytanie_id, odp, poprawna])
+                db.execute(sql, [None, pytanie_id, odp, poprawna])
+            db.commit()
+            flash(f'Dodano pytanie: {pytanie}')
+            return redirect(url_for('pytania.lista'))
+
+        # jeżeli formularz zawiera błędy
+        pytanie_form['pytanie'] = pytanie
         pytanie_form['odpowiedzi'] = odpowiedzi
 
-    return render_template('pytania/pytanie_dodaj.html', errors=errors, pytanie_form=pytanie_form)
+    return render_template('pytania/pytanie_dodaj.html', errors=errors, pytanie_form=pytanie_form, akcja='Dodaj')
+
 # sql = 'SELECT p.*, o.* FROM pytanie p INNER JOIN odpowiedz o WHERE p.id=o.pytanie_id'
